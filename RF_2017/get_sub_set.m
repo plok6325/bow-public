f2d7428f5fed885919996cbda619454c12274b1a
plok6325 <zhanghongbingod@outlook.com>
2017-03-25 21:22:47 +0000

    first commit

--- RF_2017/get_sub_set.m
+++
@@ -1,8 +0,0 @@
-function [ sub_data ] = get_sub_set( data ,r )
-% from data set. select equal number of instances . with replacement 
-[m,n]= size (data);
-
- idx = randsample(m,m,r);
-sub_data = data(idx,:);
- 
-end
